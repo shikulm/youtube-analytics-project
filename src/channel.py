0171b152c3fb8b66f483733596f078ad24e61c9a
Михаил Шикульский <shikul_m@mail.ru>
2023-04-22 01:43:40 +0400

    add __init__() and print_infor() into class Channel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -5,8 +10,13 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+        api_key: str = os.getenv('YT_API_KEY')
+        # создать специальный объект для работы с API
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
