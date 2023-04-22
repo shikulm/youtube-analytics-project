9c4e4b0f0d15ec9f1d96a93abdb327977b65e455
Михаил Шикульский <shikul_m@mail.ru>
2023-04-22 19:14:21 +0400

    add attributes with description Channel into __init__() for class Channel

--- src/channel.py
+++ src/channel.py
@@ -7,16 +7,29 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    api_key: str = os.getenv('YT_API_KEY')
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-        api_key: str = os.getenv('YT_API_KEY')
-        # создать специальный объект для работы с API
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.channel_id = channel_id
+        self.channel = Channel.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
+
+if __name__ == '__main__':
+    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    vdud.print_info()
