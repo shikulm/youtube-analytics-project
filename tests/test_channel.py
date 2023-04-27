af3d02e2695858c44edb14e5a46f6e8054831785
Михаил Шикульский <shikul_m@mail.ru>
2023-04-27 18:21:11 +0400

    add test__gt__() and test__gt__ValueError() for class Channel

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -82,6 +82,16 @@ def test__le__ValueError(vdud):
     with pytest.raises(ValueError):
         vdud <= 5
 
+
+def test__gt__(vdud, redactsiya):
+    """Тест >"""
+    assert (vdud > redactsiya) == (vdud.subscriber_count > redactsiya.subscriber_count)
+
+
+def test__gt__ValueError(vdud):
+    with pytest.raises(ValueError):
+        vdud > 5
+
 def test_property_without_setter(vdud):
     with pytest.raises(AttributeError):
         vdud.channel_id = 'Новое название'
