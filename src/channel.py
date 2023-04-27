31de7387653cacea6efd0722fd573d6c57bd45a2
Михаил Шикульский <shikul_m@mail.ru>
2023-04-27 18:13:39 +0400

    add __ne__(self, other) for class Channel

--- src/channel.py
+++ src/channel.py
@@ -42,6 +42,11 @@ class Channel:
             raise ValueError('Неправильный тип данных аргумента')
         return self.subscriber_count == other.subscriber_count
 
+    def __ne__(self, other):
+        if not isinstance(other, Channel):
+            raise ValueError('Неправильный тип данных аргумента')
+        return self.subscriber_count != other.subscriber_count
+
     @classmethod
     def get_service(cls):
         """класс-метод get_service(), возвращающий объект для работы с YouTube API"""
