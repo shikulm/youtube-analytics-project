5268039607b5624f519c652c70d5767e04529ad6
Михаил Шикульский <shikul_m@mail.ru>
2023-05-05 22:37:08 +0400

    add show_best_video() and test_show_best_video() for class PlayList

--- src/playList.py
+++ src/playList.py
@@ -1,11 +1,12 @@
 from src.video import PLVideo
-
+import datetime
 
 class PlayList:
 
     # Полчаем ссылку на API youtube
     youtube = PLVideo.youtube
-    def __init__(self, playlist_id):
+
+    def __init__(self, playlist_id: str) -> None:
         """
         Инициализация свойств плэйлиста по его id
         """
@@ -44,7 +45,20 @@ class PlayList:
             self.playlist_videos.append(PLVideo(playlist['contentDetails']['videoId'], self.playlist_id))
             # print(playlist)
 
+    @property
+    def total_duration(self) -> datetime.timedelta:
+        """Возвращает объект класса datetime.timedelta с суммарной длительность плейлиста"""
+        return sum([el.duration for el in self.playlist_videos], datetime.timedelta())
+
+    def show_best_video(self):
+        """Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)"""
+        max_video_like_count = max([el.video_like_count for el in self.playlist_videos])
+        # print(max_video_like_count)
+        return [el.video_url for el in self.playlist_videos if el.video_like_count == max_video_like_count][0]
+
 
 
 pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
 print(pl.playlist_videos)
+print(pl.total_duration)
+print(pl.show_best_video())
--- tests/test_playList.py
+++ tests/test_playList.py
@@ -1,6 +1,7 @@
 import pytest
 from src.playList import PlayList
 from src.video import PLVideo
+import datetime
 
 @pytest.fixture()
 def pl():
@@ -16,5 +17,18 @@ def test_playlist_videos(pl):
     assert len(pl.playlist_videos) > 0
     assert isinstance(pl.playlist_videos, list)
     assert isinstance(pl.playlist_videos[0], PLVideo)
+
+
+def test_total_duration(pl):
+    """Тестирование метода total_duration"""
+    duration = pl.total_duration
+    assert str(duration) == "3:41:01"
+    assert isinstance(duration, datetime.timedelta)
+    assert duration.total_seconds() == 13261.0
+
+def test_show_best_video(pl):
+    assert pl.show_best_video() == "https://youtu.be/9Bv2zltQKQA"
+
+
 
 
